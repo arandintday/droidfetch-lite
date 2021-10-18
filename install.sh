66ab303033f076633923de0d0e19f98711402649
arandintday <lzchappytime2@gmail.com>
2021-10-18 15:18:47 +0800

    fix some bugs

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-mkdir $HOME/.config/droidfetch
+if $(mkdir $HOME/.config/droidfetch > /dev/null 2>&1); then
+    :
+else
+    mkdir -vp $HOME/.config/droidfetch
+    echo "[!] W:It seems you hasn't installed neofetch, continue installation anyway."
+    echo "[*] You should run pkg install neofetch first."
+fi
 cp -r ./ascii $HOME/.config/droidfetch/
 chmod +x ./droidfetch
 cp ./droidfetch $HOME/../usr/bin/
